173385d45df9b974897fc50bb03f65c326ec9d07
iamwatchdogs <shamith301102@gmail.com>
2023-05-28 17:26:56 +0530

    Adding Codex API as the Backend

--- core.php
+++ core.php
@@ -3,9 +3,52 @@
 // For handling`POST` Request 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    
+    // if there is no input parameter in POST request, we'll pass out empty string.
+    if (!isset($_POST['input'])){
+        $_POST['input'] = "";
+    };
+
+    // Checking content in the POST request
     echo "<pre>";
     var_dump($_POST);
+    echo "</pre>";
+    
+    // Converting POST request into URL-encoded query string.
+    $payload = http_build_query($_POST);
+
+    // Initializing cURL request to the Codex API.
+    $curl = curl_init();
+    
+    // Setting cURL options.
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api.codex.jaagrav.in',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $payload,
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/x-www-form-urlencoded'
+        ),
+    ));
+    
+    // Executing the cURL Request though CodeX API.
+    $response = curl_exec($curl);
+    
+    // Basic Error Handling
+    if (curl_errno($curl)) {
+        echo 'Error: ' . curl_error($curl);
+    }
+    
+    // Safely closing cURL connection.
+    curl_close($curl);
+
+    // Displaying response content.
+    echo "<pre>";
+    var_dump($response);
     echo "</pre>";
 }
 ?>
